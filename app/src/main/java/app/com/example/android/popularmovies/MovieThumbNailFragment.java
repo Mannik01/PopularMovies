2df3931ca7a5d95acb0c3495e04fd91722af1cb7
MandipSilwal <mannik456@gmail.com>
2016-05-26 23:37:05 -0500

    initialized repository

---
+++ app/src/main/java/app/com/example/android/popularmovies/MovieThumbNailFragment.java
@@ -0,0 +1,23 @@
+package app.com.example.android.popularmovies;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class MovieThumbNailFragment extends Fragment {
+
+    public MovieThumbNailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
