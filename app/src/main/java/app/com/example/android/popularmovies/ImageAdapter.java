4ba90f04f674404b546d0e2b0b700d93bfc501d4
MandipSilwal <mannik456@gmail.com>
2016-06-19 20:19:42 -0500

    updated values files for settings functionality

--- app/src/main/java/app/com/example/android/popularmovies/ImageAdapter.java
+++ app/src/main/java/app/com/example/android/popularmovies/ImageAdapter.java
@@ -1,7 +1,81 @@
 package app.com.example.android.popularmovies;
 
+import android.content.Context;
+import android.graphics.drawable.Drawable;
+import android.provider.ContactsContract;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.ImageView;
+
+import com.squareup.picasso.Picasso;
+
+import java.util.ArrayList;
+
 /**
  * Created by MandipSilwal on 6/13/16.
  */
-public class ImageAdapter {
+public class ImageAdapter extends BaseAdapter {
+    Context context;
+    ArrayList<String> paths;
+
+    public ImageAdapter(Context c, ArrayList<String> pathsAPI) {
+        this.context = c;
+        paths = pathsAPI;
+    }
+
+    @Override
+    public int getCount() {
+        return paths.size();
+    }
+
+    @Override
+    public Object getItem(int position) {
+        return paths.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return position;
+    }
+
+    private class ViewHolder {
+
+        ImageView mImage;
+
+        public ViewHolder(ImageView view) {
+            mImage = view;
+        }
+
+        public ImageView getmImage() {
+            return mImage;
+        }
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        ImageView imageView = (ImageView) convertView;
+        ViewHolder holder = null;
+
+        if(imageView == null) {
+            LayoutInflater inflater = (LayoutInflater) parent.getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            imageView = (ImageView) inflater.inflate(R.layout.grid_item_thumbnail,parent,false);
+            holder = new ViewHolder(imageView);
+            imageView.setTag(holder);
+        }
+        else {
+            holder = (ViewHolder) imageView.getTag();
+        }
+
+        Picasso.
+                with(context)
+                .cancelRequest(holder.getmImage());
+        Picasso
+                .with(context)
+                .load("http://image.tmdb.org/t/p/w185/" + paths.get(position))
+                .into(holder.getmImage());
+        return imageView;
+    }
+
 }
--- app/src/main/java/app/com/example/android/popularmovies/MovieThumbNailFragment.java
+++ app/src/main/java/app/com/example/android/popularmovies/MovieThumbNailFragment.java
@@ -1,27 +1,44 @@
 package app.com.example.android.popularmovies;
 
-import android.content.Context;
+import android.content.SharedPreferences;
 import android.media.Image;
+import android.os.AsyncTask;
+import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.support.v4.content.res.TypedArrayUtils;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
+import android.widget.AdapterView;
 import android.widget.GridView;
-import android.widget.ImageView;
 
-import com.squareup.picasso.Picasso;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.prefs.PreferenceChangeListener;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MovieThumbNailFragment extends Fragment {
 
+    ArrayList<String> pathList;
+    ImageAdapter imageAdapter;
+    GridView gridView;
+    String sortType;
+    SharedPreferences prefs;
+    PreferenceChangeListener listener;
+    String prevSort;
 
     public MovieThumbNailFragment() {
     }
@@ -37,68 +54,167 @@ public class MovieThumbNailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        ArrayList<String> paths = new ArrayList<String>();
-
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
-        GridView gridview = (GridView) rootView.findViewById(R.id.gridView);
-        ImageAdapter adapter = new ImageAdapter(getActivity(), paths);
-        gridview.setAdapter(adapter);
+        gridView = (GridView) rootView.findViewById(R.id.gridView);
+        pathList = new ArrayList<String>();
+        imageAdapter = new ImageAdapter(getActivity(), pathList);
+        gridView.setAdapter(imageAdapter);
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+            }
+        });
         return rootView;
     }
 
+//    private class PreferenceChangeListener implements SharedPreferences.OnSharedPreferenceChangeListener{
+//
+//
+//        @Override
+//        public void onSharedPreferenceChanged(SharedPreferences sharedPreferences, String key) {
+//            gridView.setAdapter(null);
+//            onStart();
+//        }
+//    }
 
-    class ImageAdapter extends BaseAdapter {
-        Context context;
-        LayoutInflater inflater;
-        ArrayList<String> pathList;
+    @Override
+    public void onStart() {
+        super.onStart();
+        populateView();
+    }
 
+    @Override
+    public void onResume() {
+        prefs = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortfield = prefs.getString(getString(R.string.pref_sort_key), getString(R.string.pref_sort_popularity));
 
-        public ImageAdapter(Context c, ArrayList<String> paths) {
-            this.pathList = paths;
-            this.context = c;
-            inflater = LayoutInflater.from(c);
+        if(prevSort!=null && !sortfield.equals(prevSort)) {
+            populateView();
         }
+        prevSort = sortfield;
+        super.onResume();
+    }
+
+    private void populateView() {
+        FetchImageTask imageTask = new FetchImageTask();
+        SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
+        String sortType = preferences.getString(getString(R.string.pref_sort_key), getString(R.string.pref_sort_label_popularity));
+        imageTask.execute(sortType);
+    }
+
+
+    public class FetchImageTask extends AsyncTask<String, Void, ArrayList<String>> {
 
         @Override
-        public int getCount() {
-            return pathList.size();
-        }
+        protected ArrayList<String> doInBackground(String... params) {
 
-        @Override
-        public Object getItem(int position) {
+            if (params.length == 0) {
+                return null;
+            }
+
+            String url = "http://api.themoviedb.org/3/discover/movie/?sort_by=" + params[0] + ".desc&api_key=" + BuildConfig.THE_MOVIE_DB;
+
+            try {
+                pathList.clear();
+                pathList = new ArrayList<String>(Arrays.asList(getMoviesPaths(url)));
+                return pathList;
+            } catch (Exception e) {
+
+            }
             return null;
         }
 
-        @Override
-        public long getItemId(int position) {
-            return 0;
-        }
+        private String[] getMoviesPaths(String builtUrl) {
 
+            StringBuilder stringBuilder = new StringBuilder();
+            InputStream inputStream = null;
+            HttpURLConnection urlConnection = null;
+            BufferedReader bufferedReader = null;
+            String moviesInfo = null;
+            URL url = null;
 
-        @Override
-        public View getView(int position, View convertView, ViewGroup parent) {
+            try {
+                url = new URL(builtUrl);
+                urlConnection = (HttpURLConnection) url.openConnection();
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
 
+                inputStream = urlConnection.getInputStream();
+                if (inputStream == null) {
+                    moviesInfo = null;
+                }
 
-            if(convertView == null) {
-                //LayoutInflater inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-                convertView = inflater.inflate(R.layout.grid_item_thumbnail, parent, false);
-                //holder = new ViewHolder(gridItem);
-                //gridItem.setTag(holder);
+                bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+
+                String line;
+                while ((line = bufferedReader.readLine()) != null) {
+                    stringBuilder.append(line + "\n");
+                }
+
+                if (stringBuilder.length() == 0) {
+                    return null;
+                }
+
+                moviesInfo = stringBuilder.toString();
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (urlConnection != null) {
+                    urlConnection.disconnect();
+                }
+
+                if (bufferedReader != null) {
+                    try {
+                        bufferedReader.close();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
             }
-            //else {
-               // holder = (ViewHolder) gridItem.getTag();
-            //}
+            try {
 
-            //holder.thumbnail.setImageResource(imageIds.get(position));
-            Picasso
-            .with(context)
-            //.load(imageIds.get(position))
-            .load("http://api.themoviedb.org/3/movie/popular?api_key=THE_MOVIE_DB")
-            .into((ImageView) convertView);
-            return convertView;
+                return getMoviesPathsJSON(moviesInfo);
+            } catch (JSONException e) {
+                e.printStackTrace();
+            }
+            return null;
         }
-    }
 
+        private String[] getMoviesPathsJSON(String moviesInfo) throws JSONException {
+            JSONObject jsonReader = new JSONObject(moviesInfo);
+
+            JSONArray jsonArray = jsonReader.getJSONArray("results");
+            int jsonArrayLen = jsonArray.length();
+            String[] pathsArr = new String[jsonArrayLen];
+
+            JSONObject aMovie;
+            for (int i = 0; i < jsonArrayLen; i++) {
+                aMovie = jsonArray.getJSONObject(i);
+                pathsArr[i] = aMovie.getString("poster_path");
+            }
+            return pathsArr;
+        }
+
+        @Override
+        protected void onPostExecute(ArrayList<String> pathsResult) {
+
+
+            if (pathsResult != null && getActivity() != null) {
+                imageAdapter.notifyDataSetChanged();
+                imageAdapter = new ImageAdapter(getActivity(), pathsResult);
+                gridView = (GridView) getView().findViewById(R.id.gridView);
+                gridView.setAdapter(imageAdapter);
+//                imageAdapter.notifyDataSetChanged();
+//                gridView.setAdapter(imageAdapter);
+            }
+
+        }
+
+
+    }
 
 }
 
